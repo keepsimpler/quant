d1bcda58ba9b3dca2d3ce7efcff48836d6514426
Wenfeng Feng <fengwenfeng@gmail.com>
2015-12-01 00:04:49 +0000

    add distribution of species densities in equilibrium

--- R/ecology.acm.R
+++ R/ecology.acm.R
@@ -37,50 +37,74 @@ get_intrinsic_growth_rates <- function(params, nstar) {
   })
 }
 
-# get all length 2 paths of antagonism interactions
-GammaAN2 <- function(AN, AP) {
+#' @title get sum of resource species of species j which is consumer of species i
+#' @param AN, the Antagonism Negative(species j is consumer of speices i) interactions
+#' @param AP, the Antagonism Positive(species j is resource of species i) interactions
+#' @param nstar, the densities of species in equilibrium
+#' @return a matrix, its i-row j-col element is the sum of resource species of species j which is consumer of species i
+GammaAN2 <- function(AN, AP, nstar) {
   n = dim(AP)[1]
   N2 = matrix(0, nrow = n, ncol = n)
   for (i in 1:n) {
     for (j in 1:n) {
       if (AN[i, j] > 0) # j is the consumer of i
-        N2[i, j] = sum(AP[j, ]) # sum of the resource species of j
+        N2[i, j] = sum(AP[j, ] * nstar) # sum of the resource species of j
     }
   }
   N2
 }
 
-get_jacobian <- function(coeff,  antago.symm = FALSE) {
-  nstar = rep(1, coeff['s']) # assign species densities in equilibrium
+#' @title get Jacobian matrix in equilibrium
+#' @param coeff, coefficients
+#' @param antago.symm, is it symmetric the antagonism interaction strengths
+#' @param nstar, densities of species in equilibrium
+get_jacobian <- function(coeff,  antago.symm = FALSE, nstar = NULL) {
+  if (is.null(nstar))
+    nstar = rep(1, coeff['s']) # assign species densities in equilibrium to 1
   hybrid_graph <- gen_hybrid_network(coeff['s'], coeff['k'], pc = coeff['pc'], pa = coeff['pa'], pm = coeff['pm'])
   params <- params_acm(hybrid_graph, coeff, antago.symm = antago.symm)
   #params$r <- get_intrinsic_growth_rates(params, nstar)
 
   h = unlist(coeff['h.mu'])
   # diagonal elements of Jacobian matrix equal to ...
-  AN2 <- GammaAN2(AN = params$AN, AP = params$AP)
-  GammaDh <- rowSums(h * params$AN * t(params$AP) / (1 + h * AN2)^2)
-  GammaD <- - params$s + GammaDh
+  AN2 <- GammaAN2(AN = params$AN, AP = params$AP, nstar = nstar)
+  #GammaDh <- rowSums(h * params$AN * t(params$AP) / (1 + h * AN2)^2)
+  GammaDh <- diag(nstar) %*% (h * params$AN * t(params$AP) / (1 + h * AN2)^2) %*% nstar
+  GammaD <- - params$s * nstar + GammaDh
   # non-diagonal elements of Jacobian matrix equal to ...
   e = unlist(coeff['e.mu'])
   g = unlist(coeff['g.mu'])
-  GammaM <- diag(1 / ((1  + h * rowSums(params$M))^2)) %*% (e * params$M)
-  GammaAP <- diag(1 / ((1  + h * rowSums(params$AP))^2)) %*% (g * params$AP)
-  GammaAN <- - params$AN  / (1 + h * AN2)
-  GammaC <- - params$C
+  #GammaM <- diag(1 / ((1  + h * rowSums(params$M))^2)) %*% (e * params$M)
+  GammaM <- diag(nstar / ((1  + h * (params$M %*% nstar))^2)) %*% (e * params$M)
+  #GammaAP <- diag(1 / ((1  + h * rowSums(params$AP))^2)) %*% (g * params$AP)
+  GammaAP <- diag(nstar / ((1  + h * (params$AP %*% nstar))^2)) %*% (g * params$AP)
+  #GammaAN <- - params$AN  / (1 + h * AN2)
+  GammaAN <- - diag(nstar) %*% params$AN  / (1 + h * AN2)
+  #GammaC <- - params$C
+  GammaC <- - diag(nstar) %*% params$C
   Gamma <- GammaM + GammaAP + GammaAN + GammaC + diag(GammaD)
 }
 
 myfun3 <- function(coeff, antago.symm = FALSE) {
   print(coeff['id'])
-  Phi <- get_jacobian(coeff = coeff, antago.symm = antago.symm)
-  Eii <- mean(diag(Phi))
-  Eij <- mean(Phi)
-  Eij2 <- mean(Phi*Phi)
-  Eijji <- mean(Phi * t(Phi))
+  s = unlist(coeff['s'])
+  nstar = runif(s, min = 0, max = 2)
+  Phi <- get_jacobian(coeff = coeff, antago.symm = antago.symm, nstar = nstar)
+  Eii <- mean(diag(Phi))  # mean of diagonal elements
+  Vii <- var(diag(Phi))  # variance of diagonal elements
+  Eij <- mean(Phi[row(Phi) != col(Phi)])  # mean of off-diagonal elements
+  Phi2 <- Phi * Phi
+  Eij2 <- mean(Phi2[row(Phi2) != col(Phi2)]) # mean of squre of off-diagonal elements
+  Phi2 <- Phi * t(Phi)
+  Eijji <- mean(Phi2[row(Phi2) != col(Phi2)]) # mean of multiplication of Eij and Eji
   eigenvalues = eigen(Phi)$values
   lev = max(Re(eigenvalues))
-  c(coeff = coeff, lev = lev, Eii = Eii, Eij = Eij, Eij2 = Eij2, Eijji = Eijji)
+
+  I = diag(1, coeff['s']) # identity matrix
+  Vs = 1 / (2 * norm(- solve(kronecker(I, Phi) + kronecker(Phi, I)), type = '2'))
+  Vd = 1 / norm(- solve(Phi), type = '2')
+  R0 = - max(Re(eigen(Phi + t(Phi))$values)) / 2
+  c(coeff = coeff, lev = lev, Eii = Eii, Vii = Vii, Eij = Eij, Eij2 = Eij2, Eijji = Eijji, Vd = Vd, Vs = Vs, R0 = R0)
 }
 
 # Spectral Norm of matrix (we only consider real matrix here!)
