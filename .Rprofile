c4c68fb17b1909fd869a454eeb0803d777a6a044
keepsimpler <fengwenfeng@gmail.com>
2023-03-08 09:56:02 +0800

    reinitialize

--- .Rprofile
+++
@@ -1,4 +0,0 @@
-cat("Welcome to this project.\n")
-require(QuantR)
-require(zoo)
-require(xts)
--- Ref/bookDMwR.R
+++
@@ -1,6 +0,0 @@
-######### Book: Data mining with R - Learning with Case Studies
-
-##### Charpter 3 - Predicting stock market returns
-
-# 3.2.1 - Handling time-dependent Data
-x1 <- xts(rnorm(100), seq(as.POSIXct("2000-01-01"), len = 100, by = "day"))
